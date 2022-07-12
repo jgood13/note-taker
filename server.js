1ff350309aca102e93e309e2c92c1c859e9bcda5
jgood13 <joseph.good8@gmail.com>
2022-07-12 13:21:46 -0600

    got the get request working for notes

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const randomId = require('random-id')
+const len = 10
+const pattern = 'aA0'
 
 const app = express()
-const PORT = 3030
+const PORT = 3001
 
 app.use(express.urlencoded({extended: true}))
 app.use (express.json())
-
+// middleware
 app.use(express.static('./Develop/public'))
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/db/db.json'));
+});
+
+app.post('/notes', function(req,res){
+  let db = fs.readFile('./Develop/db/db.json')
+  db =JSON.parse(db)
+  res.json(db)
+  let 
+
+})
+
+
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
